667bf2068a08e27e985123cbcaeb990b65bf758d
RAVIDHOTE <ravidhote6593@gmail.com>
2022-09-18 23:01:51 +0530

    First commit....

---
+++ src/git/for_team.java
@@ -0,0 +1,14 @@
+package git;
+
+public class for_team {
+	
+	
+	public void detail(String name) {
+		System.out.println(name);
+	}
+
+	public static void main(String [] args) {
+		for_team y = new for_team();
+		y.detail("Ravi");
+	}
+}
